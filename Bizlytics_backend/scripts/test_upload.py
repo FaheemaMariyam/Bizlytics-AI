0609a15cdde399f9e077db223f09041035c50604
Faheema Mariyam <faheemamariyam2102004@gmail.com>
2026-04-21 11:19:55 +0530

    chore: remove scripts from git tracking

--- Bizlytics_backend/scripts/test_upload.py
+++
@@ -1,7 +0,0 @@
-from storage.s3_service import upload_file_to_s3
-
-file_path = "test.csv"
-
-with open(file_path, "rb") as f:
-    url = upload_file_to_s3(f, "test.csv", "text/csv")
-    print("Uploaded URL:", url)
